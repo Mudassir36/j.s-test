e8348b468bd5895f398571be68b709961f751456
Mudassir36 <mk5208694@gmail.com>
2024-10-11 18:46:03 +0500

    Add files via upload

---
+++ Test_Batch_12_js/app.js
@@ -0,0 +1,59 @@
+
+    //Q1
+
+// var name1 = prompt("Enter Your Name")
+// var age = prompt("Enter Your Age")
+// alert("Hello, " + name1 +"! " + "You are " + age + " Years Old.")
+
+
+
+
+    //  Q2
+
+
+// var numb = prompt("Enter The Number")
+// if(numb > 0){
+//     alert("The number is positive")
+// } else if (numb < 0){
+//     alert("The number is negative")
+// } else (
+//     alert("The number is zero")
+// )
+
+
+
+
+
+
+    // Q3
+
+
+
+var score=[0,0,0,0,0];
+var sum = 0;
+
+for(var i = 1; i <= 5; i++){
+var number = +prompt("enter marks of student " + i);
+score[i] = number;
+}
+for(var i = 0; i<score.length; i++){
+  sum = sum + score[i];
+}
+
+var avg = sum/5
+alert("The average score is "+ avg);
+
+
+
+
+
+
+            //    Q4
+
+// var city=["karachi","lahore","islamabad","hyderabad","peshawar"]
+// for(var i = 0; i< city.length; i++){
+//     alert("city "+ (i+1) +" : " + city[i])
+//  }
+
+
+
